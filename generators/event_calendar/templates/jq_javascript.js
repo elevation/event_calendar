a26b6985d1f4ff362008559d132a57baf9b02ac2
Robert Berry <berrydigital@gmail.com>
2009-10-20 15:49:56 -0700

    fixed the jquery template to respond only to the event dom object with mouse event.

--- generators/event_calendar/templates/jq_javascript.js
+++ generators/event_calendar/templates/jq_javascript.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $(".event").mouseenter(function() {
-    $(".event").css("background-color", "#2EAC6A");
+    $(this).css("background-color", "#2EAC6A");
     return false;
   })
   $(".event").mouseleave(function() {
-    $(".event").css("background-color", "#9aa4ad");
+    $(this).css("background-color", "#9aa4ad");
     return false;
   })
 
