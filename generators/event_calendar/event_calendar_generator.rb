eb490c1387a117d58ed9f36338fe65c4ef6ddd57
Robert Berry <berrydigital@gmail.com>
2009-10-20 01:24:01 -0700

    Added JQuery template for generation with trailing "jq" on the generator

--- generators/event_calendar/event_calendar_generator.rb
+++ generators/event_calendar/event_calendar_generator.rb
@@ -16,8 +16,11 @@ class EventCalendarGenerator < Rails::Generator::Base
     record do |m|
       # static files
       m.file "stylesheet.css", "public/stylesheets/event_calendar.css"
-      m.file "javascript.js", "public/javascripts/event_calendar.js"
-      m.directory "public/images/event_calendar"
+      
+			script = ['jquery', 'jq'].include?(ARGV[0]) ? 'jq_javascript.js' : 'javascript.js'
+		  m.file script, "public/javascripts/event_calendar.js"
+      
+			m.directory "public/images/event_calendar"
       m.file "85_bg.gif", "public/images/event_calendar/85_bg.gif"
       m.file "120_bg.gif", "public/images/event_calendar/120_bg.gif"
       
---
+++ generators/event_calendar/templates/jq_javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".event").mouseenter(function() {
+    $(".event").css("background-color", "#2EAC6A");
+    return false;
+  })
+  $(".event").mouseleave(function() {
+    $(".event").css("background-color", "#9aa4ad");
+    return false;
+  })
+
+
+})
+
