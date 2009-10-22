66b4f08eb93b702c19dd73b42ba51594135936c0
Jeff Schuil <jeff@yourelevation.com>
2009-10-21 19:21:36 -0700

    Generate the files given in the example, by default.

--- generators/event_calendar/event_calendar_generator.rb
+++ generators/event_calendar/event_calendar_generator.rb
@@ -1,11 +1,45 @@
+require File.expand_path(File.dirname(__FILE__) + "/lib/insert_routes.rb")
+
 class EventCalendarGenerator < Rails::Generator::Base
+  default_options :static_only => false
+  
+  attr_reader :class_name, :view_name
+  
+  def initialize(args, options = {})
+    super
+    usage if args.length > 0 and args.length < 2
+    @class_name = (args.shift || "event").underscore
+    @view_name = (args.shift || "calendar").underscore
+  end
+  
   def manifest
     record do |m|
+      # static files
       m.file "stylesheet.css", "public/stylesheets/event_calendar.css"
       m.file "javascript.js", "public/javascripts/event_calendar.js"
       m.directory "public/images/event_calendar"
       m.file "85_bg.gif", "public/images/event_calendar/85_bg.gif"
       m.file "120_bg.gif", "public/images/event_calendar/120_bg.gif"
+      
+      # MVC and other supporting files
+      unless options[:static_only]
+        m.template "model.rb.erb", File.join("app/models", "#{@class_name}.rb")
+        m.template "controller.rb.erb", File.join("app/controllers", "#{@view_name}_controller.rb")
+        m.directory File.join("app/views", @view_name)
+        m.template "view.html.erb", File.join("app/views", @view_name, "index.html.erb")
+        m.template "helper.rb.erb", File.join("app/helpers", "#{@view_name}_helper.rb")        
+        m.migration_template "migration.rb.erb", "db/migrate", :migration_file_name => "create_#{@class_name.pluralize}"
+        m.route_name(@view_name, "/#{@view_name}/:year/:month", ":controller => '#{@view_name}', :action => 'index', :year => Time.zone.now.year, :month => Time.zone.now.month")
+      end
     end
+  end
+  
+  protected
+  
+  def add_options!(opt)
+    opt.separator ''
+    opt.separator 'Options:'
+    opt.on("--static-only",
+      "Only generate the static files. (stylesheet, javascript, and images)") { |v| options[:static_only] = v }
   end
 end
